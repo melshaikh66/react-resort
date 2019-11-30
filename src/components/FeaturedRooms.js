5c13fb3e839527d0de9ab42f01e1a379295cba8a
Mostafa Elshaikh <m.elshaikh66@gmail.com>
2019-11-30 21:59:00 +0300

    single room structure

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 import { RoomContext } from "../Context";
+import Loading from "./Loading";
+import Title from "./Title";
+import Room from "./Room";
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    const value = this.context;
-    console.log(value);
+    let { loading, featuredRooms: rooms } = this.context;
+    rooms = rooms.map(room => {
+      return <Room key={room.id} room={room} />;
+    });
 
-    return <div>hello from featured rooms {value} </div>;
+    return (
+      <section className="featured-rooms">
+        <Title title="featured rooms" />
+        <div className="featured-rooms-center">
+          {loading ? <Loading /> : rooms}
+        </div>
+      </section>
+    );
   }
 }
