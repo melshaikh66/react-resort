004b8d2334fddc2905962466936ab8ff4133874c
Mostafa Elshaikh <m.elshaikh66@gmail.com>
2019-12-12 14:39:40 +0300

    filter &list

---
+++ src/components/RoomContainer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import RoomsFilter from "./RoomsFilter";
+import RoomsList from "./RoomsList";
+import { withRoomConsumer } from "../Context";
+import Loading from "./Loading";
+
+function RoomContainer({ context }) {
+  const { loading, sortedRooms, rooms } = context;
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <div>
+      hello from room container
+      <RoomsFilter rooms={rooms} />
+      <RoomsList rooms={sortedRooms} />
+    </div>
+  );
+}
+export default withRoomConsumer(RoomContainer);
